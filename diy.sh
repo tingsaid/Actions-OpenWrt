781fa8d7bd2aa91d43d370169e8d2cc98b5c4ab0
Tingsaid <13901263+tingsaid@users.noreply.github.com>
2026-07-25 12:03:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,3 +15,5 @@ echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 echo 'luci-theme-argon'
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+
